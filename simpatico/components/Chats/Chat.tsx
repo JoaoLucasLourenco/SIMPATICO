1380ed483b24378b6bcd1c9f3ea9ba54f680183b
João Lucas Lourenço <118781114+JoaoLucasLourenco@users.noreply.github.com>
2025-04-23 14:02:54 +0000

    create: Selected Course and Discipline chat display

--- simpatico/components/Chats/Chat.tsx
+++ simpatico/components/Chats/Chat.tsx
@@ -1,9 +1,25 @@
-import { Grid } from "@mui/material";
+"use client";
+
+import { useCourse } from "@/lib/context/useCourse";
+import { Grid, Typography } from "@mui/material";
 
 export const Chat = () => {
+  const { selectedCourse, selectedDiscipline } = useCourse();
   return (
     <Grid spacing={2} container alignSelf={"center"} justifyContent={"center"}>
-      <Grid size={{ lg: 8, md: 8, sm: 11, xs: 11 }}>CHAT</Grid>
+      <Grid size={{ lg: 8, md: 8, sm: 11, xs: 11 }} textAlign={"center"} my={5}>
+        <Typography
+          color="info"
+          variant="h5"
+          component={"h1"}
+          fontWeight={"bold"}
+        >
+          {selectedCourse}
+        </Typography>
+        <Typography color="text.secondary" variant="h6" component={"h2"}>
+          {selectedDiscipline}
+        </Typography>
+      </Grid>
     </Grid>
   );
 };
